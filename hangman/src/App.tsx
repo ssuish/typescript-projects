5d168b1257b24646f56d5d10789dd6233a87cb8a
ssuish <quidor.adrean@gmail.com>
2025-02-14 12:09:17 +0800

    feat: implement Hangman game components and replace word list with a new JSON file

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+import words from "./assets/wordList.json";
+import { HangmanDrawing } from "./hangmanDrawing";
+import { HangmanWord } from "./hangmanWord";
+import { Keyboard } from "./keyboard";
+
 function App() {
-  
-  return <h1>Hello World</h1>
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+        <HangmanDrawing />
+        <HangmanWord />
+        <div
+          style={{
+            alignSelf: "stretch",
+          }}
+        >
+          <Keyboard />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ hangman/src/hangmanWord.tsx
@@ -0,0 +1,35 @@
+export function HangmanWord() {
+  const word = "test";
+  const guessedLetters = ["e"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span
+          style={{
+            borderBottom: ".1em solid black",
+          }} key={index}
+        >
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
